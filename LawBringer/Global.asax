40beb47ce2c033712ed7f32191b6f8ca05553d8f
jjstuba2391 <32370963+jjstuba2391@users.noreply.github.com>
2017-10-31 11:47:48 -0500

    Add project files.

---
+++ LawBringer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LawBringer.MvcApplication" Language="C#" %>
---
+++ LawBringer/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace LawBringer
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            // When applicatoon starts, route config is what will "hijack" inbound requesta and direct them to your controller
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
