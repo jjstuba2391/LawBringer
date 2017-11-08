3aabadb25fbccf3aee6fd878e3087c92f3ff4932
jjstuba2391 <32370963+jjstuba2391@users.noreply.github.com>
2017-11-08 12:50:32 -0600

    added new tables

---
+++ LawDoggs/dbo/Tables/Lawyer.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Lawyer]
+(
+	[Id] INT IDENTITY NOT NULL,
+	[Name] NVARCHAR(100),
+	[City] NVARCHAR(100),
+	[Price] MONEY,
+	[Image] NVARCHAR(100),
+	[HelpTypeID] INT, 
+	[TimeSlot] DateTime,
+	CONSTRAINT FK_Lawyer_HelpType FOREIGN KEY (HelpTypeID) REFERENCES HelpType(ID)
+	
+)
